90cadfe2e2a0f5c06055c34bf3f471b545aaa073
Vinit Shah <vinshah333@gmail.com>
2023-06-18 16:14:40 +0530

    Add files via upload

---
+++ File Converter/script.js
@@ -0,0 +1,18 @@
+const textarea = document.querySelector("textarea"),
+fileNameInput = document.querySelector(".file-name input"),
+selectMenu = document.querySelector(".save-as select"),
+saveBtn = document.querySelector(".save-btn");
+
+selectMenu.addEventListener("change", () => {
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save As ${selectedOption.split(" ")[0]} File`;
+})
+
+saveBtn.addEventListener("click", () => {
+    const blob = new Blob([textarea.value], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a"); 
+    link.download = fileNameInput.value; 
+    link.href = fileUrl; 
+    link.click(); 
+})
